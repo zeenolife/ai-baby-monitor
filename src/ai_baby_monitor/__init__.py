9a7d3b0974ff4129e55a09c05b0bfd1e0529607b
zeenolife <almaz.zinollayev@gmail.com>
2025-02-28 11:06:35 +0000

    uv packaging

--- src/ai_baby_monitor/__init__.py
+++ src/ai_baby_monitor/__init__.py
@@ -1,6 +1,5 @@
 from .stream.camera_stream import CameraStream, Frame
 from .stream.redis_stream import RedisStreamHandler
 from .instructions import Instruction
-from .watcher import Watcher
 
-__all__ = ["CameraStream", "Frame", "Instruction", "Watcher", "RedisStreamHandler"]
+__all__ = ["CameraStream", "Frame", "Instruction", "RedisStreamHandler"]
